efa7df04128f96e153faf9c36cd262b8be2274f4
Ivan De Menezes <ivandmenezes@gmail.com>
2018-10-21 17:03:19 -0400

    Added MQTT

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
 const defaultTimeOut = 60000; // Tiempo en milisegundos para volver al estado OFF
 const active = []; // Botones con cuenta regresiva activa
+const client = mqtt.connect({
+    host: '',
+    port: 1883
+});
 
-function switchOn(event, time = 1) {
+function switchOn(event, time = 1, topicRoot, subTopic) {
     const button = event.target;
+    const topic = `/${topicRoot}/${subTopic}`;
 
     if (!active.includes(button.id) && button !== null) {
         button.innerHTML = 'ON';
         button.classList.add('on');
 
         active.push(button.id);
+
+        if (client.connected) {
+            client.publish(topic, 'ON');
+        }
+
         const timeToOff = defaultTimeOut * time;
-        
-        setTimeout(switchOff, timeToOff, button);
+        setTimeout(switchOff, timeToOff, button, topic);
     }
 }
 
-function switchOff(button) {
+function switchOff(button, topic) {
     if (button !== null) {
         button.innerHTML = 'OFF';
 
         if (button.classList.contains('on')) {
             button.classList.remove('on');
+        }
+
+        if (client.connected) {
+            client.publish(topic, 'OFF');
         }
 
         const index = active.indexOf(button.id);
